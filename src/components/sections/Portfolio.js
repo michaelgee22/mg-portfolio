e4fdcee55090adfa9c204a1192cc0cd7e18ecb22
Michael Gee <michaelgee221@gmail.com>
2018-06-07 16:25:18 -0400

    Portfolio Section structure/styling redone

--- src/components/sections/Portfolio.js
+++ src/components/sections/Portfolio.js
@@ -1,45 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 
-import ProjectDialog from '../ProjectDialog';
 import Icon from '../Icon';
-import { GITHUB } from '../IconList';
+import { GITHUB, GO_ARROW, OPEN_BOOK } from '../IconList';
+
+import cegmag from '../../images/cegmag.png';
 
 const projects = [
   {
-    id: 'project-0',
-    title: 'Portfolio',
-    meta: 'React',
-    codeHref: 'https://github.com/michaelgee22/portfolio-react'
+    id: 'cegmag',
+    className:'project',
+    title: 'CEGMAG Solutions Inc',
+    tags: [ 'JavaScript', 'Polymer', 'HTML5', 'CSS3', 'Git' ],
+    codeHref: false,
+    href: 'https://website-eeb2b.firebaseapp.com/',
+    image: cegmag,
+    imageAlt: 'CEGMAG Solutions Inc. Project'
   },
   {
-    id: 'project-1',
-    title: 'CEGMAG Solutions Inc.',
-    meta: 'Polymer',
-    codeHref: false
+    id: 'portfolio',
+    className: 'projectReverse',
+    title: 'Portfolio (Current Application)',
+    tags: ['JavaScript', 'React', 'HTML5', 'CSS3', 'Git'],
+    codeHref: '',
+    href: false,
+    image: cegmag,
+    imageAlt: 'Portfolio Project'
   },
   {
-    id: 'project-2',
-    title: 'Chat-Box',
-    meta: 'Polymer & Firebase',
-    codeHref: 'https://github.com/michaelgee22/chat-box'
+    id: 'chat-box',
+    className: 'project',
+    title: 'Chat-box',
+    tags: ['JavaScript', 'Polymer', 'Firebase', 'HTML5', 'CSS3', 'Git'],
+    codeHref: '',
+    href: 'https://mgchatbox.com/',
+    image: cegmag,
+    imageAlt: 'Chat-Box Project'
   },
   {
-    id: 'project-3',
+    id: 'lp-tribute',
+    className: 'projectReverse',
     title: 'Linkin Park Tribute',
-    meta: 'Vue & Firebase',
-    codeHref: 'https://github.com/michaelgee22/lp_tribute'
-  },
-  {
-    id: 'project-4',
-    title: 'Rough Draft Portfolio',
-    meta: 'Polymer',
-    codeHref: 'https://github.com/michaelgee22/portfolio'
-  },
+    tags: ['JavaScript', 'Vue', 'Firebase', 'HTML5', 'CSS3', 'Git'],
+    codeHref: '',
+    href: 'https://website-eeb2b.firebaseapp.com/',
+    image: cegmag,
+    imageAlt: 'Linkin Park Project'
+  }
 ];
 
 const styles = {
@@ -50,110 +61,108 @@ const styles = {
     textAlign: 'center'
   },
   projectsContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around'
+    
   },
   project: {
-    width: '300px',
-    marginBottom: '25px',
-    boxShadow: '0 1px 4px 0 rgba(0, 0, 0, 0.14)',
-    borderRadius: '3px',
-    backgroundColor: '#3F51B5',
-    textAlign: 'center'
-  },
-  projectTitle: {
-    margin: '25px 0 10px 0',
-    fontSize: '2em',
-    fontWeight: 500,
-    color: 'white'
-  },
-  projectMeta: {
-    fontSize: '1.6em',
-    color: 'rgba(255, 255, 255, 0.8)',
-    marginBottom: '15px'
-  },
-  projectBtns: {
     display: 'flex',
     justifyContent: 'space-around',
-    marginBottom: '15px'
+    paddingBottom: '50px'
   },
-  previewBtn: { color: 'white' }
+  projectReverse: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    paddingBottom: '50px',
+    flexDirection: 'row-reverse'
+  },
+  projectImgWrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '50%'
+  },
+  projectImg: {
+    width: '350px',
+    height: '350px',
+    borderRadius: '50%',
+    border: '10px solid #3F51B5'
+  },
+  projectInfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%'
+  },
+  projectTitle: {
+    fontSize: '2em',
+    fontWeight: 500,
+    margin: '15px 0'
+  },
+  projectTags: {
+    margin: '15px 0',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  tag: {
+    boxShadow: '0 1px 4px 0 rgba(0, 0, 0, 0.14)',
+    fontSize: '1.3em',
+    fontWeight: 500,
+    padding: '6px 15px',
+    margin: '0 15px 15px 0',
+    border: '1px solid #f5f5f5'
+  },
+  projectBtn: {
+    backgroundColor: '#3F51B5',
+    margin: '0 5px',
+
+    "&:hover": {
+      backgroundColor: 'black'
+    }
+  }
 };
 
-class Portfolio extends Component {
-  constructor(props) {
-    super(props);
+const Portfolio = (props) => {
+  const { classes } = props;
 
-    this.state = { 
-      open: false,
-      currentProject: { }
-    };
+  return (
+    <div>
+      <h2 className={classes.sectionTitle}>Portfolio</h2>
 
-    this.handleDialogOpen = this.handleDialogOpen.bind(this);
-    this.handleDialogClose = this.handleDialogClose.bind(this);
-  }
+      <div className={classes.projectsContainer}>
+        {projects.map(item => {
+          return <div key={item.id} className={classes[item.className]}>
 
-  handleDialogOpen(e) {
-    let index = e.currentTarget.id.replace(/^\D+/g, '');
-    index = parseInt(index);
+            <div className={classes.projectImgWrapper}>
+              <img src={item.image} className={classes.projectImg} alt={item.imageAlt} />
+            </div>
 
-    console.log(projects[index]);
-
-    this.setState({ open: true, currentProject: projects[index] });
-  };
-
-  handleDialogClose = value => {
-    this.setState({ open: false });
-  };
-
-  renderIconButton(href) {
-    if(!href) {
-      return <IconButton disabled>
-        <Icon icon={GITHUB} />
-      </IconButton>
-    }
-
-    return <IconButton href={href} target="_blank">
-      <Icon icon={GITHUB} />
-    </IconButton>
-  }
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <div>
-        <h2 className={classes.sectionTitle}>Portfolio</h2>
-
-        <div className={classes.projectsContainer}>
-          {projects.map(item => {
-            return <div key={item.id} className={classes.project}>
+            <div className={classes.projectInfo}>
               <h3 className={classes.projectTitle}>{item.title}</h3>
-              <div className={classes.projectMeta}>{item.meta}</div>
+              <div className={classes.projectTags}>
+                {item.tags.map(tag => {
+                  return <div className={classes.tag} key={tag}>{tag}</div>
+                })}
+              </div>
 
-              <div className={classes.projectBtns}>
-                {this.renderIconButton(item.codeHref)}
+              <div>
+                <IconButton href={item.codeHref} target="_blank" className={classes.projectBtn}>
+                  <Icon icon={GITHUB} />
+                </IconButton>
 
-                <Button 
-                  id={item.id}
-                  className={classes.previewBtn}
-                  onClick={this.handleDialogOpen}
-                >
-                Preview
-                </Button>
-                <ProjectDialog 
-                  currentProject={this.state.currentProject}
-                  open={this.state.open}
-                  onClose={this.handleDialogClose}
-                />
+                <IconButton className={classes.projectBtn}>
+                  <Icon icon={OPEN_BOOK} />
+                </IconButton>
+
+                <IconButton href={item.href} target="_blank" className={classes.projectBtn}>
+                  <Icon icon={GO_ARROW} />
+                </IconButton>
               </div>
             </div>
-          })}
-        </div>
+          </div>
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Portfolio.propTypes = {
