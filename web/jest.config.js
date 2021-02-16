4f09b181e6aa5e4c455b1d40a86822b1f0fce9a9
Michael Gee <michaelgee221@gmail.com>
2021-02-15 19:30:02 -0500

    fix broken module name mapper in jest config

--- web/jest.config.js
+++ web/jest.config.js
@@ -7,5 +7,14 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@/containers/(.*)$': '<rootDir>/src/containers/$1',
+    '^@/hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@/services/(.*)$': '<rootDir>/src/services/$1',
+    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
 }
